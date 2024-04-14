06bce6b1e77179423c017794e9fdac0f39405e80
armsves <armsves@gmail.com>
2024-04-14 11:38:47 +0200

    final push

--- contracts/oERC20.sol
+++ contracts/oERC20.sol
@@ -1,8 +1,12 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract oERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    }
+
     struct HolderData {
         bool isBalanceRevealed;
         uint256 revealEndTime;
@@ -12,16 +16,16 @@ contract oERC20 is ERC20 {
 
     function revealBalance(uint256 revealTime) public {
         _holderData[msg.sender].isBalanceRevealed = true;
-        _holderData[msg.sender].revealEndTime = now + revealTime;
+        _holderData[msg.sender].revealEndTime = block.timestamp + revealTime;
     }
 
     function revokeBalance() public {
         _holderData[msg.sender].isBalanceRevealed = false;
     }
 
-    function balanceOf(address account) public view returns (uint256) {
+    function balanceOf(address account) public override view returns (uint256) {
         HolderData storage data = _holderData[account];
-        bool currentlyRevealed = data.isBalanceRevealed && now <= data.revealEndTime;
+        bool currentlyRevealed = data.isBalanceRevealed && block.timestamp <= data.revealEndTime;
         return currentlyRevealed ? super.balanceOf(account) : 0;
     }
 }
